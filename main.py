d920993827bb7d109d2bdac9e886745521180ea6
Nabeel Okal <nabeel.okal@gmail.com>
2024-03-13 07:14:07 +0300

    UPDATED!!

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def board(board):
-    return board[0] + ' | ' + board[1] + ' | ' + board[2] + '\n----------\n' + board[3] + ' | ' + board[4] + ' | ' + board[5] + '\n----------\n' + board[6] + ' | ' + board[7] + ' | ' + board[8]
+    print(board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('---------')
+    print(board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('---------')
+    print(board[6] + ' | ' + board[7] + ' | ' + board[8])
+
 
 def position_choice():
     '''
@@ -9,30 +14,64 @@ def position_choice():
     choice = 'Wrong'
     while choice not in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
         choice = input('Enter position (0-8): ')
-        if choice not in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
-            print('Invalid input.')
+    if choice not in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
+        print('Invalid input.')
     return int(choice)
+
 
 def user_replacement(board, pos, symbol):
     board[pos] = symbol
     return board
 
 if __name__ == '__main__':
-    game_list = board([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+
+    game_list = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    # game_list = board(['@', '@', '@', '@', '@', '@', '@', '@', '@'])
     print('Welcome to Tic Tac Toe!')
     print('Here is the current board.')
-    print(game_list)
-
+    board(game_list)
 
     first_player_symbol = 'NULL'
     while first_player_symbol not in ['X', 'O']:
-        first_player_symbol = input('Enter your symbol (first player): ')
-        if first_player_symbol not in ['X', 'O']:
-            print('Invalid input.')
+        first_player_symbol = input('Enter your symbol (first player): ').upper()
+    if first_player_symbol not in ['X', 'O']:
+        print('Invalid input.')
     pos1 = position_choice()
-    game_list = user_replacement(game_list, pos1, first_player_symbol)
 
-    second_player_symbol = 'X' if first_player_symbol == 'O' else 'O'
+    user_replacement(game_list, pos1, first_player_symbol)
 
-    print(board(game_list))
+    board(game_list)
 
+
+second_player_symbol = 'X' if first_player_symbol == 'O' else 'O'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+def display_board(board):
+    print(board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('---------')
+    print(board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('---------')
+    print(board[6] + ' | ' + board[7] + ' | ' + board[8])
+
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+display_board(test_board)
+'''
